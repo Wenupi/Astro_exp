c2da207388b36912b1fd0a47395c05a6c712f396
Wenupi <71337338+Wenupi@users.noreply.github.com>
2021-05-28 02:39:57 -0400

    Corrección curva de rotación
    Se estaban usando unidades distintas jaja

--- Tarea2/codigo/T2.py
+++ Tarea2/codigo/T2.py
@@ -10,13 +10,14 @@ from scipy import ndimage
 import pandas as pd  # tablas y manejo de datos
 from astropy.stats import sigma_clip
 from scipy.optimize import curve_fit  # fitear curvas
+from astropy import units as u
 from matplotlib.font_manager import FontProperties  # fuente de los gráficos
 import math
 
 G = 4.302e-6  # [km3kg-1s-2] creo
 cubo = fits.open("southgal_fixbadc.fits")  # se abre el cubo de datos
 data = cubo[0].data  # extracción matriz de datos
-header= cubo[0].header  # extracción del header
+header = cubo[0].header  # extracción del header
 
 
 def values(h,j):
@@ -33,9 +34,9 @@ def values(h,j):
 
 # Estos serán los tres arreglos con los valores reales de los tres ejes
 # del cubo
-velocidad=values(header,1)
-longitud=values(header,2)
-latitud=values(header,3)
+velocidad = values(header,1)
+longitud = values(header,2)
+latitud = values(header,3)
 """
 # Índices de una longitud y latitud específica
 lon_i = np.where(longitud== 325.0)[0][0]
@@ -111,9 +112,10 @@ def fmin(l, latitud, vs):
 vmin = np.zeros(385)
 bvmin = np.zeros(385)
 R = np.zeros(385)
-R0 = 8.5  # kPc
-vsol = 220  # km/s
-omegasol = vsol/R0
+#R0 = 8.5*10**3*3.086e+13  # [kPc]->[km]
+R0 = 8.5*10**3*u.parsec  # [pc]
+vsol = 220*u.kilometer/u.second  # [km/s]
+omegasol = vsol/(R0.to(u.kilometer))
 
 # maximorum
 # Se recorren las longitudes y se busca la velocidad más negativa (mayor en modulo), se guarda esta
@@ -132,7 +134,7 @@ for i in range(385):
             b1 = latitud[j+1]
     vmin[i] = v1
     bvmin[i] = b1
-    R[i] = np.abs(R0*sp.sin(longitud[i]*sp.pi/180.))  # R0 sin(l)
+    R[i] = np.abs((R0.to(u.kilometer)).value*sp.sin(longitud[i]*sp.pi/180.))  # R0 sin(l) [km]
 
     
 # Se obtiene la Vtan con Vtan = −Vmin − Vsol · sin(lπ/180 ), donde Vmin es la velocidad mayor en
@@ -143,19 +145,24 @@ vR = np.zeros(385)
 for i in range(385):
     vR[i] = vmin[i]*(np.abs(sp.sin(longitud[i]*sp.pi/180.))/\
                      sp.sin(longitud[i]*sp.pi/180.)) +\
-                     np.abs(vsol*sp.sin(longitud[i]*sp.pi/180.))
+                     np.abs((vsol.value)*sp.sin(longitud[i]*sp.pi/180.))
+
+#R = R*3.2408e-14*1e-3
+#R0 = R0*3.2408e-14*1e-3
 
 # %%
 """
 ======================P1======================
 """
+t = 3.2408*10**(-14)*10**(-3)  # kilometro -> kilo parsec
+
 #curva de rotacion
 fig2, ax2 = plt.subplots(2, 1, figsize=(5.5, 5), sharex=True)
 fig2.subplots_adjust(hspace=0)
-ax2[0].plot(R, vR, 'maroon')
-ax2[0].plot(R, vR, 'r.')
-ax2[1].plot(R, vR/R+omegasol, 'maroon')
-ax2[1].plot(R, vR/R+omegasol, 'r.')
+ax2[0].plot(R*t, vR, 'maroon')
+ax2[0].plot(R*t, vR, 'r.')
+ax2[1].plot(R*t, vR/R+omegasol.value, 'maroon')
+ax2[1].plot(R*t, vR/R+omegasol.value, 'r.')
 ax2[1].grid()
 ax2[0].set_ylim(50, 255)
 ax2[0].set_yticks(np.arange(60, 255, 40))
@@ -173,12 +180,12 @@ fig2.savefig("img/curva_rotacion.pdf")
 ======================P2======================
 """
 def altura_z(l, b_max):
-    return R0*np.cos(l*sp.pi/180.)*np.tan(b_max*sp.pi/180.)
+    return R0.value*np.cos(l*sp.pi/180.)*np.tan(b_max*sp.pi/180.)  #*1e-3*3.24078e-14
 
 l_list = np.arange(0, 385)
-fig4, ax4 = plt.subplots(figsize=(5.5, 2.5))
-ax4.scatter(R, altura_z(l_list, bvmin), color='blue', marker='.')
-ax4.plot(R, altura_z(l_list, bvmin), color='green', linewidth=0.5)
+fig4, ax4 = plt.subplots(figsize=(5.3, 2.5))
+ax4.scatter(R*t, altura_z(longitud, bvmin)*10**(-3), color='blue', marker='.')
+ax4.plot(R*t, altura_z(longitud, bvmin)*10**(-3), color='green', linewidth=0.5)
 ax4.set_xlabel('R [kpc]')
 ax4.set_ylabel('z [kpc]')
 ax4.grid()
@@ -191,7 +198,13 @@ fig4.savefig("img/corrugacion.pdf")
 """
 ======================P3======================
 """
+#G = 4.302e-6
+G_1 = 6.67408*1e-11*u.meter**3*u.kilogram**(-1)*u.second**(-2) # m3 kg-1 s-2
+G_2 = G_1.to(u.parsec**3*u.kilogram**(-1)*u.second**(-2))  # km3 kg-1 s-2
+G = G_2.value
 G = 4.302e-6
+
+t_2 = 3.086*10**(13)  # parsec -> kilometro
 # Funciones de la velocidad tangencial para cada modelo
 def distribucion_masa_1(r, M_0):
     return np.sqrt(G*M_0/r)
@@ -213,20 +226,20 @@ def distribucion_masa_5(r, rho, M_0):
     return np.sqrt(G*(4/3*np.pi*r**3*rho + M_0)/r)
 
 
-popt_1, pcov_1 = curve_fit(distribucion_masa_1, R, vR)
-popt_2, pcov_2 = curve_fit(distribucion_masa_2, R, vR)
-popt_3, pcov_3 = curve_fit(distribucion_masa_3, R, vR)
-popt_4, pcov_4 = curve_fit(distribucion_masa_4, R, vR)
-popt_5, pcov_5 = curve_fit(distribucion_masa_5, R, vR)
+popt_1, pcov_1 = curve_fit(distribucion_masa_1, R*t, vR, bounds=(0, [np.inf]))
+popt_2, pcov_2 = curve_fit(distribucion_masa_2, R*t, vR, bounds=(0, [np.inf]))
+popt_3, pcov_3 = curve_fit(distribucion_masa_3, R*t, vR, bounds=(0, [np.inf]))
+popt_4, pcov_4 = curve_fit(distribucion_masa_4, R*t, vR, bounds=(0, [np.inf, np.inf]))
+popt_5, pcov_5 = curve_fit(distribucion_masa_5, R*t, vR, bounds=(0, [np.inf, np.inf]))
 
 font = FontProperties()
 font.set_family('serif')  # fuente para los plots
 
 fig3, ax3 = plt.subplots(5, 1, figsize=(5.2, 9), sharex=True)
 fig3.subplots_adjust(hspace=0)
-ax3[0].plot(R, distribucion_masa_1(R, *popt_1))
-ax3[0].plot(R, vR, 'maroon')
-ax3[0].plot(R, vR, 'r.')
+ax3[0].plot(R*t, distribucion_masa_1(R*t, *popt_1))
+ax3[0].plot(R*t, vR, 'maroon')
+ax3[0].plot(R*t, vR, 'r.')
 ax3[0].set_ylim(50, 255)
 ax3[0].set_yticks(np.arange(60, 255, 40))
 ax3[0].grid()
@@ -240,9 +253,9 @@ ax3[0].text(0.9, 0.15, '$M_0$='+str(format(popt_1[0],'.3E')), fontsize=8.7,
          verticalalignment='center', transform=ax3[0].transAxes,
          bbox=dict(facecolor='white', alpha=1))
 """
-ax3[1].plot(R, distribucion_masa_2(R, *popt_2))
-ax3[1].plot(R, vR, 'maroon')
-ax3[1].plot(R, vR, 'r.')
+ax3[1].plot(R*t, distribucion_masa_2(R*t, *popt_2))
+ax3[1].plot(R*t, vR, 'maroon')
+ax3[1].plot(R*t, vR, 'r.')
 ax3[1].set_ylim(50, 255)
 ax3[1].set_yticks(np.arange(60, 255, 40))
 ax3[1].grid()
@@ -256,9 +269,9 @@ ax3[1].text(0.8, 0.3, 'S='+str(popt_2[0]), fontsize=11,
          verticalalignment='center', transform=ax3[1].transAxes,
          bbox=dict(facecolor='white', alpha=1))
 """
-ax3[2].plot(R, distribucion_masa_3(R, *popt_3))
-ax3[2].plot(R, vR, 'maroon')
-ax3[2].plot(R, vR, 'r.')
+ax3[2].plot(R*t, distribucion_masa_3(R*t, *popt_3))
+ax3[2].plot(R*t, vR, 'maroon')
+ax3[2].plot(R*t, vR, 'r.')
 ax3[2].set_ylim(50, 255)
 ax3[2].set_yticks(np.arange(60, 255, 40))
 ax3[2].grid()
@@ -273,9 +286,9 @@ ax3[2].text(0.8, 0.3, r'$\rho=$'+str(popt_3[0]), fontsize=11,
          verticalalignment='center', transform=ax3[2].transAxes,
          bbox=dict(facecolor='white', alpha=1))
 """
-ax3[3].plot(R, distribucion_masa_4(R, *popt_4))
-ax3[3].plot(R, vR, 'maroon')
-ax3[3].plot(R, vR, 'r.')
+ax3[3].plot(R*t, distribucion_masa_4(R*t, *popt_4))
+ax3[3].plot(R*t, vR, 'maroon')
+ax3[3].plot(R*t, vR, 'r.')
 ax3[3].set_ylim(50, 255)
 ax3[3].set_yticks(np.arange(60, 255, 40))
 ax3[3].grid()
@@ -289,9 +302,9 @@ ax3[3].text(0.76, 0.3, 'S='+str(popt_4[0])+'$M_0=$'+str(popt_4[1]), fontsize=11,
          verticalalignment='center', transform=ax3[3].transAxes,
          bbox=dict(facecolor='white', alpha=1))
 """
-ax3[4].plot(R, distribucion_masa_5(R, *popt_5))
-ax3[4].plot(R, vR, 'maroon')
-ax3[4].plot(R, vR, 'r.')
+ax3[4].plot(R*t, distribucion_masa_5(R*t, *popt_5))
+ax3[4].plot(R*t, vR, 'maroon')
+ax3[4].plot(R*t, vR, 'r.')
 ax3[4].set_ylim(50, 255)
 ax3[4].set_yticks(np.arange(60, 255, 40))
 ax3[4].grid()
